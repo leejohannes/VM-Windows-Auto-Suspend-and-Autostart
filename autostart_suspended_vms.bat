6e9da835aae923033dec01117f714d44f54d573e
leejohannes <leeyoo@leeyoo.org>
2024-03-31 17:12:22 +0800

    Update autostart_suspended_vms.bat
    作为启动脚本运行生成的log文件不在脚本所在的目录
    会在win的C:\WINDOWS\System32\GroupPolicy\Machine\Scripts\Startup
    中生成。。。
    备注掉的是锁屏界面的指令。。。

--- autostart_suspended_vms.bat
+++ autostart_suspended_vms.bat
@@ -1,6 +1,7 @@
-echo starting %date%-%time% >> suspend_log.txt
+cd "%~dp0%"
+echo autostart starting %date%-%time% >> suspend_log.txt"
 for /f "tokens=* skip=1" %%a in (suspended.txt) do (
-timeout /t 60
 "C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" start %%a
 )
+::Rundll32.exe user32.dll,LockWorkStation
 echo autostart finished %date%-%time% >> suspend_log.txt

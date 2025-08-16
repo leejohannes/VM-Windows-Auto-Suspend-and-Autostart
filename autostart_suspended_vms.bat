6ba5f021d93a2efebdf75671477ac3abf940f986
leejohannes <leeyoo@leeyoo.org>
2025-08-16 21:53:20 +0800

    Delete autostart_suspended_vms.bat

--- autostart_suspended_vms.bat
+++
@@ -1,7 +0,0 @@
-cd "%~dp0%"
-echo autostart starting %date%-%time% >> suspend_log.txt"
-for /f "tokens=* skip=1" %%a in (suspended.txt) do (
-"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" start %%a
-)
-::Rundll32.exe user32.dll,LockWorkStation
-echo autostart finished %date%-%time% >> suspend_log.txt
